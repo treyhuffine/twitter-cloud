f3949e20d55e022ee92dafd3f1ba8a4b6cad4e3e
Trey Huffine <treyhuffine@gmail.com>
2015-07-07 16:45:52 -0700

    Prepare to hit real Twitter API

--- source/index.js
+++ source/index.js
@@ -3,7 +3,14 @@
 var app = angular.module('twitterCloud', ['ui.router']);
 
 app.controller('MainCtrl', function($scope, $state, $http) {
-  $http.get('http://localhost:8001/search').success(function(data) {
-    $scope.tags = data;
-  })
+  $scope.tags = [];
+  $scope.search = function() {
+    $http.post('http://localhost:8001/search', { words: $scope.words })
+    .success(function(data) {
+      $scope.tags = data;
+    });
+  };
+  // $http.get('http://localhost:8001/search').success(function(data) {
+  //   $scope.tags = data;
+  // });
 });
