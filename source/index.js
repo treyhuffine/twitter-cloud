55e1a24da7d731547fc6e3c4cdf075451fa55f9f
Trey Huffine <treyhuffine@gmail.com>
2015-07-07 18:09:13 -0700

    Success send Tweet

--- source/index.js
+++ source/index.js
@@ -4,13 +4,21 @@ var app = angular.module('twitterCloud', ['ui.router']);
 
 app.controller('MainCtrl', function($scope, $state, $http) {
   $scope.tags = [];
+  $scope.tweet = "";
   $scope.search = function() {
     $http.post('http://localhost:8001/search', { words: $scope.words })
     .success(function(data) {
       $scope.tags = data;
     });
   };
-  // $http.get('http://localhost:8001/search').success(function(data) {
-  //   $scope.tags = data;
-  // });
+  $scope.sendTweet = function() {
+    $http.post('http://localhost:8001/sendtweet', {tweet: $scope.tweet})
+      .success(function(data) {
+        console.log(data);
+        $scope.tweet = "";
+      });
+  };
+  $scope.includeInTweet = function(tag) {
+    $scope.tweet = $scope.tweet + " " + tag;
+  };
 });
